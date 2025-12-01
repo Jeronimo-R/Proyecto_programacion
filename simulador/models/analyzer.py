3452066a3f195fd23733f1bb70368660af937da7
danyuru <johandtp1@gmail.com>
2025-12-01 15:07:13 -0500

    Delete simulador directory

--- simulador/models/analyzer.py
+++
@@ -1,9 +0,0 @@
-class Analyzer:
-    def detect_anomaly(self, measurement, expected):
-        difference = measurement.power - expected
-
-        if measurement.power > expected * 1.35:
-            severity = difference / expected
-            return True, severity 
-
-        return False, 0.0
